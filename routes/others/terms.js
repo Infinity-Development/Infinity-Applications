e7bd077a9951afb501ff8dce06cbd8ea50e7fd81
Rootspring <cheesycod@outlook.com>
2023-01-21 12:35:20 +0530

    Update terms.js

--- routes/others/terms.js
+++ routes/others/terms.js
@@ -1,13 +1,8 @@
 const { Router } = require('express');
 const route = Router();
 
-const config = require('../../configs/index');
-const staffList = require('../../configs/staff');
-
-const { newPage } = require('../../utilitys/newPage');
-
 route.get('/', async (req, res) => {
-  newPage(res, req, 'others/terms.ejs', { alert: null, error: null });
+  res.redirect('https://infinitybots.gg/terms');
 });
 
 module.exports = route;
