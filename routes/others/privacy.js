566bbd78dce808f8437c4668d80b8fb9269ecaba
Rootspring <cheesycod@outlook.com>
2023-01-21 12:34:42 +0530

    Update privacy.js

--- routes/others/privacy.js
+++ routes/others/privacy.js
@@ -1,13 +1,8 @@
 const { Router } = require('express');
 const route = Router();
 
-const config = require('../../configs/index');
-const staffList = require('../../configs/staff');
-
-const { newPage } = require('../../utilitys/newPage');
-
 route.get('/', async (req, res) => {
-  newPage(res, req, 'others/privacy.ejs', { alert: null, error: null });
+  res.redirect('https://infinitybots.gg/privacy');
 });
 
 module.exports = route;
